45b26aef7579f403166f4d8649aa759584bb9ea4
mskat <zur.katarzyna@gmail.com>
2020-10-06 11:29:29 +0100

    Add the class with test

---
+++ src/test/java/pl/generator/GeneratorAppTest.java
@@ -0,0 +1,18 @@
+package pl.generator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class GeneratorAppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
