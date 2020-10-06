30e3b5b770e6f376df96c941839c89fe019270d3
mskat <zur.katarzyna@gmail.com>
2020-10-06 11:09:56 +0100

    Create MemberList class

---
+++ src/main/java/pl/generator/constant/MemberList.java
@@ -0,0 +1,31 @@
+package pl.generator.constant;
+
+import pl.generator.model.Member;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberList {
+    private List<Member> allMembersList = new ArrayList<>();
+
+    // Getter
+    public List<Member> getAllMembersList() throws IOException {
+        String file = "members.txt";
+        String line;
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(file));
+            while ((line = br.readLine()) != null) {
+                String[] member = line.split(",");
+                Member newMember = new Member(member[0], member[1], Level.valueOf(member[2]));
+                allMembersList.add(newMember);
+            }
+        } finally {
+            if (br != null) {
+                br.close();
+            }
+        }
+        return allMembersList;
+    }
+}
